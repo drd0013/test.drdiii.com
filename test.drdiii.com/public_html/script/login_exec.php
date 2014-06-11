1c8d34d39187711441bb0394b871b16edc181752
Rob D <rob.davis@drdiii.com>
2014-06-11 17:47:55 -0400

    login_exec WORKING

--- test.drdiii.com/public_html/script/login_exec.php
+++ test.drdiii.com/public_html/script/login_exec.php
@@ -1,37 +1,40 @@
 <?php
-include('../includes.connection.php');
+include('../includes/connection.php');
 
 $esc_email = mysqli_real_escape_string($con, $_POST['email']);
 $esc_pw = mysqli_real_escape_string($con, $_POST['password']);
 
 $get_salt = 
-    "SELECT salt
+    "SELECT salt, password
     FROM userlogin
     WHERE email = '$esc_email'";
 
 $find_user = 
-    "SELECT email
+    "SELECT cust_id
     FROM userlogin
     WHERE email = '$esc_email' AND
     password = '$hash_pw'";
-
-if($res = mysqli_query($con, $get_salt)){
-    $row = mysqli_fetch_assoc($res);
-    $salt = $row['salt'];
+$res = mysqli_query($con, $get_salt);
+if($row = mysqli_fetch_row($res)){
+    $salt = $row[0];
     $salt_pw = $esc_pw . $salt;
     $hash_pw = hash('sha256', $salt_pw);
     }
 else{
     mysqli_close($con);
-    header("location: /login.php?remarks=nfd1");
+    header("location: ../login.php?remarks=nfd1");
+	exit;
     }
-if(mysqli_query($con, $find_user)){
-    setcookie('email', $esc_email, false, '/user', 'test.drdiii.com');
+/*$res = mysqli_query($con, $find_user);*/
+if($hash_pw == $row[1]){
+    /*setcookie('email', $esc_email, false, '/user', 'test.drdiii.com');*/
     mysqli_close($con);
-    header("Location: ./Account/index.php");
+    header("Location: ../home/index.php");
+	exit;
     }
 else{
     mysqli_close($con);
-    header("location: /login.php?remarks=nfd2");
+    header("location: ../login.php?remarks=nfd2");
+	exit;
     }
 ?>
