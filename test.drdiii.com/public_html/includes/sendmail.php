dd82ce94c60aa83644f5a34a555af7c094a74854
Rob D <rob.davis@drdiii.com>
2014-06-11 12:18:12 -0400

    reg_exec WORKING

--- test.drdiii.com/public_html/includes/sendmail.php
+++ test.drdiii.com/public_html/includes/sendmail.php
@@ -1,9 +1,9 @@
 <?php
 $subject = 'Widgets Registation';
-$message = 'Hello ' . (string)$email . ' and thank you for registering your Widgets account!';
+$message = 'Hello ' . (string)$esc_email . ' and thank you for registering your Widgets account!';
 $headers = 'From: noreply@drdiii.com' . "\r\n" .
 	'Bcc: admin@drdiii.com' . "\r\n"  .
     'X-Mailer: PHP/' . phpversion();
 
-mail($email, $subject, $message, $headers);
+mail($esc_email, $subject, $message, $headers);
 ?>
--- test.drdiii.com/public_html/script/reg_exec.php
+++ test.drdiii.com/public_html/script/reg_exec.php
@@ -1,6 +1,6 @@
 <?php
 
-include('../includes.connection.php');
+include('../includes/connection.php');
 
 $esc_custid = mysqli_real_escape_string($con, $_POST['custid']);
 $esc_contact = mysqli_real_escape_string($con, $_POST['contact']);
@@ -8,14 +8,14 @@ $esc_email = mysqli_real_escape_string($con, $_POST['email']);
 $esc_pw = mysqli_real_escape_string($con, $_POST['password']);
 $salt = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
 $salt_pw = $esc_pw . $salt;
-$hash_pw = hash('sha256', $hash_pw);
+$hash_pw = hash('sha256', $salt_pw);
 $search_cust = 
-    "SELECT phone_1, phone_2
+    "SELECT phone_1, phone_2 AS tempnum
     FROM userdata
     WHERE cust_id = '$esc_custid'";
 
 $search_id =
-    "SELECT COUNT(cust_id)
+    "SELECT cust_id
     FROM userlogin
     WHERE cust_id = '$esc_custid'";
 
@@ -25,31 +25,34 @@ $create_user =
 
 
 $res = mysqli_query($con,$search_cust);
-if (mysqli_fetch_row($res)){
-    $row = mysqli_fetch_assoc($res);
-}
-else{
+if (!$row = mysqli_fetch_row($res)){
     mysqli_close($con);
     header("location: ../register.php?remarks=failcust");
+	exit;
 }
 if($esc_contact == $row[0] or $row[1]){
     $res = mysqli_query($con, $search_id);
 	if (mysqli_fetch_row($res)){
+		mysqli_close($con);
         header("location: ../register.php?remarks=failexists");
+		exit;
     }
 	elseif(mysqli_query($con,$create_user)){
-        	include('../includes.sendmail.php');
+        	include('../includes/sendmail.php');
             mysqli_close($con);
             header("location: ../register.php?remarks=success");
+			exit;
         }
 		else{
 			mysqli_close($con);
 			header("location: ../register.php?remarks=failed");
+			exit;
 	}
 }
 else{
     mysqli_close($con);
     header("location: ../register.php?remarks=failnum");
+    exit;
 }
 
 ?>
